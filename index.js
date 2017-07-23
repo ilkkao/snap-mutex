f232636afd0a7ab9cb148f7b87aac551a866f3ec
Ilkka Oksanen <iao@iki.fi>
2017-07-23 22:13:49 +0200

    Make timeout disabled by default

--- index.js
+++ index.js
@@ -7,29 +7,33 @@ class Mutex {
     this.queue = [];
   }
 
-  acquire({ timeout = 60000 } = {}) {
+  acquire({ timeout = false } = {}) {
     const queue = this.queue;
 
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const index = queue.indexOf(resolve);
+      if (timeout !== false) {
+        setTimeout(() => {
+          const index = queue.indexOf(resolve);
 
-        if (index > 0) { // Still waiting in the queue
-          queue.splice(index, 1);
-          reject('Queue timeout');
-        }
-      }, timeout);
+          if (index > 0) { // Still waiting in the queue
+            queue.splice(index, 1);
+            reject('Queue timeout');
+          }
+        }, timeout);
+      }
 
       if (queue.push(resolve) === 1) {
-        // Queue was empty, resolve the promise immediately. Caller is kept in the queue and
-        // blocks it until it caller calls release()
-        resolve({ release: function release() {
-          queue.shift();
+        // Queue was empty, resolve this promise immediately. Caller is kept in the queue until
+        // it calls release()
+        resolve({
+          release: function release() {
+            queue.shift();
 
-          if (queue.length > 0) {
-            queue[0]({ release }); // give the lock to the next in line by resolving the promise
+            if (queue.length > 0) {
+              queue[0]({ release }); // give the lock to the next in line by resolving the promise
+            }
           }
-        }});
+        });
       }
     });
   }
